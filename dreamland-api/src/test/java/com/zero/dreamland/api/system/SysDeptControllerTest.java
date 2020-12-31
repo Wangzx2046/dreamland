876fbd3c30bcc34cc8385e6d7991d5f32f7faba4
443082604@qq.com <443082604@qq.com>
2020-12-31 15:43:37 +0800

    ##预留文件

---
+++ dreamland-api/src/test/java/com/zero/dreamland/api/system/SysDeptControllerTest.java
@@ -0,0 +1,32 @@
+package com.zero.dreamland.api.system;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SysDeptControllerTest {
+
+    @Test
+    void all() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void edit() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getSuperior() {
+    }
+
+    @Test
+    void download() {
+    }
+}
