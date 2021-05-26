54d8b51aafcf3b12d5862caddee45b962bed1c35
Wang.zx <443082604@qq.com>
2021-05-26 14:46:54 +0800

    ##代码优化

--- dreamland-api/src/test/java/FakerUtils.java
+++ dreamland-api/src/test/java/FakerUtils.java
@@ -1,7 +1,7 @@
 import cn.hutool.core.util.RandomUtil;
 import cn.hutool.http.HttpUtil;
-import cn.hutool.json.JSONObject;
 import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -11,20 +11,50 @@ import java.time.ZoneOffset;
  */
 public class FakerUtils {
 
-    public static final String ACCESSDEVICES_URL = "http://edge.cosmoplat.com:30032/company/device/access";
+     public static final String ACCESSDEVICES_URL = "http://edge.cosmoplat.com:30032/company/device/access";//shengchan
+    //public static final String ACCESSDEVICES_URL = "http://edge.cosmoplat.com:30152/company/device/access";//test
 
 
     public static void main(String[] args) {
 
-        accessDevices(10, "深圳市有方科技股份有限公司" , "共享雨伞" , "GXYS" , "MQTT" , "共享设备" , "中端" ,
-                "消费品" , "广东省" , "深圳市" , "10" , "设备状态" , "status" , "1" );
 
-        accessDevices(10, "深圳市有方科技股份有限公司" , "共享充电宝" , "GXCDB" , "MQTT" , "共享设备" , "中端" ,
-                "消费品" , "广东省" , "深圳市" , "10" , "仓位数量" , "availableNum" , "24" );
+        JSONArray ja = new JSONArray();
+        JSONObject jb = new JSONObject();
+        jb.put("dataName" , "仓位数量" );
+        jb.put("mark" , "pCount" );
+        jb.put("value" , "24" );
+        ja.add(jb);
+        jb = new JSONObject();
+        jb.put("dataName" , "设备状态" );
+        jb.put("mark" , "status" );
+        jb.put("value" , "1" );
+        ja.add(jb);
+        jb = new JSONObject();
+        jb.put("dataName" , "可还数量" );
+        jb.put("mark" , "availableNum" );
+        jb.put("value" , "0" );
+        ja.add(jb);
+        jb = new JSONObject();
+        jb.put("dataName" , "可借数量" );
+        jb.put("mark" , "lendNum" );
+        jb.put("value" , "24" );
+        ja.add(jb);
+        jb = new JSONObject();
+        jb.put("dataName" , "硬件设备柜id" );
+        jb.put("mark" , "etekId" );
+        jb.put("value" , RandomUtil.randomStringUpper(12) + RandomUtil.randomNumbers(6));
+        ja.add(jb);
+
+
+        accessDevices(RandomUtil.randomInt(200,220), "深圳市有方科技股份有限公司" , "共享雨伞" , "GXYS" , "MQTT" , "其他" , "低端" ,
+                "工业品" , "广东省" , "深圳市" , "10" , ja);
+
+        accessDevices(RandomUtil.randomInt(200,250), "深圳市有方科技股份有限公司" , "共享充电宝" , "GXCDB" , "MQTT" , "其他" , "低端" ,
+                "工业品" , "广东省" , "深圳市" , "10" , ja);
     }
 
     public static void accessDevices(int num, String companyName, String productName, String productCode, String protocol, String classfy, String lever,
-                                     String dimension, String province, String city, String worth, String dataName, String mark, String value) {
+                                     String dimension, String province, String city, String worth, JSONArray ja) {
         JSONObject pjb = new JSONObject();
         pjb.put("industryName" , "山东区域" );
         pjb.put("companyName" , companyName);
@@ -40,13 +70,17 @@ public class FakerUtils {
         for (int i = 0; i < num; i++) {
             String deviceCode = "YOUFANGKEJI" + RandomUtil.randomStringUpper(4) + RandomUtil.randomNumbers(6);
             deviceArr.add(makeDevice(deviceCode, productName + deviceCode, province, city, worth));
-            deviceDataArr.add(makeDeviceData(deviceCode, dataName, mark, value));
+            ja.forEach(x -> {
+                JSONObject jbx = JSONObject.parseObject(x.toString());
+                deviceDataArr.add(makeDeviceData(deviceCode, jbx.getString("dataName" ), jbx.getString("mark" ), jbx.getString("value" )));
+            });
+
+
         }
 
 
         pjb.put("device" , deviceArr);
         pjb.put("deviceData" , deviceDataArr);
-
 
         String rel = HttpUtil.post(ACCESSDEVICES_URL, pjb.toString());
 
