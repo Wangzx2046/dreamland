0b485ca1b4af44dd233383d639cddece4c8d14bd
Wang.zx <443082604@qq.com>
2021-09-07 15:50:18 +0800

    ## 目录调整

---
+++ ZeroAct/zeroAct-admin/src/main/java/com/zero/web/controller/mobilApi/MobileLoginController.java
@@ -0,0 +1,62 @@
+package com.zero.web.controller.mobilApi;
+
+import com.alibaba.fastjson.JSONObject;
+import com.zero.common.core.domain.Result;
+import com.zero.common.core.domain.entity.SysUser;
+import com.zero.common.core.domain.model.LoginBody;
+import com.zero.common.utils.SecurityUtils;
+import com.zero.framework.web.service.SysLoginService;
+import com.zero.framework.web.service.SysPermissionService;
+import com.zero.system.service.ISysMenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 登录验证
+ *
+ * @author zero
+ */
+@RestController
+public class MobileLoginController {
+    @Autowired
+    private SysLoginService loginService;
+
+    @Autowired
+    private ISysMenuService menuService;
+
+    @Autowired
+    private SysPermissionService permissionService;
+
+    /**
+     * 登录方法
+     *
+     * @param loginBody 登录信息
+     * @return 结果
+     */
+    @PostMapping("/mlogin")
+    public Result mlogin(@RequestBody LoginBody loginBody) {
+        Result<JSONObject> result = new Result<JSONObject>();
+        // 生成令牌
+        String token = loginService.login(loginBody.getUsername(), loginBody.getPassword(), loginBody.getCode(),
+                loginBody.getUuid());
+
+        SysUser user = SecurityUtils.getLoginUser().getUser();
+
+        JSONObject obj = new JSONObject();
+
+        //用户登录信息
+        obj.put("userInfo", user);
+
+        //token 信息
+        obj.put("token", token);
+        result.setResult(obj);
+        result.setSuccess(true);
+        result.setCode(200);
+
+        return result;
+    }
+
+
+}
---
+++ ZeroGen/zeroGen-admin/src/main/java/com/zero/ZeroServletInitializer.java
@@ -0,0 +1,16 @@
+package com.zero;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * web容器中进行部署
+ *
+ * @author zero
+ */
+public class ZeroServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ZeroGenApplication.class);
+    }
+}
