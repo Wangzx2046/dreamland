df164b26296d4c434465e7b07e90ab995644add5
Wang.zx <443082604@qq.com>
2021-10-19 10:47:36 +0800

    ##

---
+++ ZeroGen/zeroGen-common/src/main/java/com/zero/common/datasource/support/DataSourceConstants.java
@@ -0,0 +1,43 @@
+
+
+package com.zero.common.datasource.support;
+
+/**
+ * @author lengleng
+ * @date 2019-04-01
+ * <p>
+ * 数据源相关常量
+ */
+public interface DataSourceConstants {
+
+	/**
+	 * 数据源名称
+	 */
+	String DS_NAME = "name";
+
+	/**
+	 * 默认驱动
+	 */
+	String DS_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+	/**
+	 * 默认数据源（master）
+	 */
+	String DS_MASTER = "master";
+
+	/**
+	 * jdbcurl
+	 */
+	String DS_JDBC_URL = "url";
+
+	/**
+	 * 用户名
+	 */
+	String DS_USER_NAME = "username";
+
+	/**
+	 * 密码
+	 */
+	String DS_USER_PWD = "password";
+
+}
