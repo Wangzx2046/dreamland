06ea3c5211cc0927ed7fc1edf27b14134cf35aea
Wang.zx <wangzhengxiang@haier.com>
2022-06-15 15:13:53 +0800

    优化商品为空问题

--- ZeroAct/zeroAct-system/src/main/java/com/zero/jdTry/domain/JdGoods.java
+++ ZeroAct/zeroAct-system/src/main/java/com/zero/jdTry/domain/JdGoods.java
@@ -13,11 +13,12 @@ import lombok.experimental.Accessors;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 
 
 /**
  * 试用商品对象 jd_goods
- * 
+ *
  * @author wzx
  * @date 2022-04-26
  */
@@ -27,151 +28,193 @@ import java.util.List;
 @EqualsAndHashCode(callSuper = true)
 @Accessors(chain = true)
 @TableName("jd_goods")
-@ApiModel(value="JdGoods对象", description="试用商品")
+@ApiModel(value = "JdGoods对象", description = "试用商品")
 public class JdGoods extends BaseEntity {
 
     private static final long serialVersionUID = 1L;
 
-    /** id */
+    /**
+     * id
+     */
 
     @ApiModelProperty(value = "${comment}")
     @TableField("id")
     private Long id;
 
-    /** 类型3种草官 5付费 */
+    /**
+     * 类型3种草官 5付费
+     */
 
     @Excel(name = "类型3种草官 5付费")
     @ApiModelProperty(value = "类型3种草官 5付费")
     @TableField("type")
     private Long type;
 
-    /**  */
+    /**
+     *
+     */
 
     @Excel(name = "")
     @ApiModelProperty(value = "")
     @TableField("sort")
     private Long sort;
 
-    /**  */
+    /**
+     *
+     */
 
     @Excel(name = "")
     @ApiModelProperty(value = "")
     @TableField("isConfig")
     private Long isConfig;
 
-    /**  */
+    /**
+     *
+     */
 
     @Excel(name = "")
     @ApiModelProperty(value = "")
     @TableField("filterType")
     private String filterType;
 
-    /** 商品编码 */
+    /**
+     * 商品编码
+     */
 
     @Excel(name = "商品编码")
     @ApiModelProperty(value = "商品编码")
     @TableField("trialActivityId")
     private Long trialActivityId;
 
-    /** 编号 */
+    /**
+     * 编号
+     */
 
     @Excel(name = "编号")
     @ApiModelProperty(value = "编号")
     @TableField("sku")
     private String sku;
 
-    /** 图片 */
+    /**
+     * 图片
+     */
 
     @Excel(name = "图片")
     @ApiModelProperty(value = "图片")
     @TableField("skuImg")
     private String skuImg;
 
-    /** 标题 */
+    /**
+     * 标题
+     */
 
     @Excel(name = "标题")
     @ApiModelProperty(value = "标题")
     @TableField("skuTitle")
     private String skuTitle;
 
-    /** 试用价格 */
+    /**
+     * 试用价格
+     */
 
     @Excel(name = "试用价格")
     @ApiModelProperty(value = "试用价格")
     @TableField("trialPrice")
     private String trialPrice;
 
-    /** 原价 */
+    /**
+     * 原价
+     */
 
     @Excel(name = "原价")
     @ApiModelProperty(value = "原价")
     @TableField("jdPrice")
-    private BigDecimal jdPrice;
+    private BigDecimal jdPrice =new BigDecimal(0);
 
-    /** 标签 */
+    /**
+     * 标签
+     */
 
     @Excel(name = "标签")
     @ApiModelProperty(value = "标签")
     @TableField("tagList")
     private List<TagList> tagList;
 
-    /** 提供数目 */
+    /**
+     * 提供数目
+     */
 
     @Excel(name = "提供数目")
     @ApiModelProperty(value = "提供数目")
     @TableField("supplyNum")
     private Long supplyNum;
 
-    /** 申请人数 */
+    /**
+     * 申请人数
+     */
 
     @Excel(name = "申请人数")
     @ApiModelProperty(value = "申请人数")
     @TableField("applyNum")
     private String applyNum;
 
-    /**  */
+    /**
+     *
+     */
 
     @Excel(name = "")
     @ApiModelProperty(value = "")
     @TableField("brokerInfo")
     private String brokerInfo;
 
-    /**  */
+    /**
+     *
+     */
 
     @Excel(name = "")
     @ApiModelProperty(value = "")
     @TableField("applyState")
     private Integer applyState;
 
-    /**  */
+    /**
+     *
+     */
 
     @Excel(name = "")
     @ApiModelProperty(value = "")
     @TableField("activityType")
     private Long activityType;
 
-    /**  */
+    /**
+     *
+     */
 
     @Excel(name = "")
     @ApiModelProperty(value = "")
     @TableField("trialProcess")
     private Long trialProcess;
 
-    /**  */
+    /**
+     *
+     */
 
     @Excel(name = "")
     @ApiModelProperty(value = "")
     @TableField("activityStatus")
     private Integer activityStatus;
 
-    /**  */
+    /**
+     *
+     */
 
     @Excel(name = "")
     @ApiModelProperty(value = "")
     @TableField("showPoint")
     private String showPoint;
 
-    /**  */
+    /**
+     *
+     */
 
     @Excel(name = "")
     @ApiModelProperty(value = "")
@@ -179,6 +222,16 @@ public class JdGoods extends BaseEntity {
     private Long isNewActivity;
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JdGoods goods = (JdGoods) o;
+        return Objects.equals(trialActivityId, goods.trialActivityId);
+    }
 
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(trialActivityId);
+    }
 }
--- ZeroAct/zeroAct-system/src/main/java/com/zero/utils/JdTryUtil.java
+++ ZeroAct/zeroAct-system/src/main/java/com/zero/utils/JdTryUtil.java
@@ -15,6 +15,7 @@ import org.json.JSONException;
 import org.springframework.util.StopWatch;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -37,7 +38,7 @@ public class JdTryUtil {
     private static final String ORIGIN = "https://prodev.m.jd.com";
     private static final String HOST = "api.m.jd.com";
     //每个Tab页要便遍历的申请页数
-    private static final int JD_TRY_TOTALPAGES = 50;
+    private static final int JD_TRY_TOTALPAGES = 30;
     //获取试用商品请求发送间隔
     private static final int REQUEST_INTERVAL = 3;
     //申请试用商品请求发送间隔
@@ -53,7 +54,9 @@ public class JdTryUtil {
     //提供的最大份数
     private static final int SUPPLY_NUM_MAX = 20;
     //获取试用商品类型
-    private static final int[] JD_TRY_TABID = {104, 3, 4, 5, 9, 10, 15, 12, 6, 7, 8, 13, 14, 11, 16};
+    private static final int[] JD_TRY_TABID = {212,104, 3, 4, 5, 9, 10, 15, 12, 6, 7, 8, 13, 14, 11, 16};
+   // private static final int[] JD_TRY_TABID = {212,208,209,210,211,202,203,201,204,205,207};
+
     //过滤的关键词
     private static final String[] INNER_KEYS = {
             "小靓美", "赠品", "MLB", "饲料", "手机电池", "样品", "面膜", "染发", "挂件", "戒烟", "宠物", "脚气", "跳蛋",
@@ -89,7 +92,8 @@ public class JdTryUtil {
         paramMap.put("body", jb.toString());
 
         String result2 = HttpRequest.post(URL)
-                .header(Header.USER_AGENT, "jdapp;android;10.5.0;;;appBuild/95837;ef/1;ep/{\"hdid\":\"JM9F1ywUPwflvMIpYPok0tt5k9kW4ArJEU3lfLhxBqw=\",\"ts\":1649917022756,\"ridx\":-1,\"cipher\":{\"sv\":\"CJO=\",\"ad\":\"DNLsCzHsCzK2DzU4DwCmYG==\",\"od\":\"YJY5EJq0CzcnCwS5Czq3Zq==\",\"ov\":\"CzK=\",\"ud\":\"DNLsCzHsCzK2DzU4DwCmYG==\"},\"ciphertype\":5,\"version\":\"1.2.0\",\"appname\":\"com.jingdong.app.mall\"};jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; MI 9 Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.72 MQQBrowser/6.2 TBS/045947 Mobile Safari/537.36")//头信息，多个头信息多次调用此方法即可
+                .header(Header.USER_AGENT, "jdapp;android;10.5.0;;;appBuild/95837;ef/1;ep/{\"hdid\":\"JM9F1ywUPwflvMIpYPok0tt5k9kW4ArJEU3lfLhxBqw=\",\"ts\":"+ Instant.now().toEpochMilli()
+                        +",\"ridx\":-1,\"cipher\":{\"sv\":\"CJO=\",\"ad\":\"DNLsCzHsCzK2DzU4DwCmYG==\",\"od\":\"YJY5EJq0CzcnCwS5Czq3Zq==\",\"ov\":\"CzK=\",\"ud\":\"DNLsCzHsCzK2DzU4DwCmYG==\"},\"ciphertype\":5,\"version\":\"1.2.0\",\"appname\":\"com.jingdong.app.mall\"};jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; MI 9 Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.72 MQQBrowser/6.2 TBS/045947 Mobile Safari/537.36")//头信息，多个头信息多次调用此方法即可
                 .header(Header.ORIGIN, ORIGIN)
                 .header(Header.HOST, HOST)
                 .header(Header.CONNECTION, "keep-alive")
@@ -104,8 +108,8 @@ public class JdTryUtil {
         Console.log(result2);
         int sleepTimes = REQUEST_APPLY + RandomUtil.randomInt(0, 55);
         if ("Error request, response status: 403".equals(result2)) {
-            TimeUnit.SECONDS.sleep(120);
             log.error("【异常】 403错误，休眠120秒后重新申请" + " " + goods.getSkuTitle());
+            TimeUnit.SECONDS.sleep(120);
             return true;
         } else {
             log.info("【申请商品】" + "休眠" + sleepTimes + "秒，价值" + goods.getJdPrice() + " " + goods.getSkuTitle());
@@ -147,11 +151,12 @@ public class JdTryUtil {
                 log.info("=================结束获取第" + JD_TRY_TABID[i] + "TAB,第" + j + "页，共计得到" + list.size() + "条有效商品=======================");
                 TimeUnit.SECONDS.sleep(REQUEST_INTERVAL);
             }
+
         }
         return list;
     }
 
-    public static List<JdGoods> getGoodsList(int tabId, int page, String cookie) {
+    public static List<JdGoods> getGoodsList(int tabId, int page, String cookie) throws InterruptedException {
         List<JdGoods> list = new ArrayList<>();
         //1.获取试用商品列表
         //链式构建请求{"tabId":"3","page":1,"version":2,"source":"default","client":"app","previewTime":""}
@@ -170,7 +175,8 @@ public class JdTryUtil {
 
 
         String result2 = HttpRequest.post(URL)
-                .header(Header.USER_AGENT, "jdapp;android;10.5.0;;;appBuild/95837;ef/1;ep/{\"hdid\":\"JM9F1ywUPwflvMIpYPok0tt5k9kW4ArJEU3lfLhxBqw=\",\"ts\":1649917022756,\"ridx\":-1,\"cipher\":{\"sv\":\"CJO=\",\"ad\":\"DNLsCzHsCzK2DzU4DwCmYG==\",\"od\":\"YJY5EJq0CzcnCwS5Czq3Zq==\",\"ov\":\"CzK=\",\"ud\":\"DNLsCzHsCzK2DzU4DwCmYG==\"},\"ciphertype\":5,\"version\":\"1.2.0\",\"appname\":\"com.jingdong.app.mall\"};jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; MI 9 Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.72 MQQBrowser/6.2 TBS/045947 Mobile Safari/537.36")//头信息，多个头信息多次调用此方法即可
+                .header(Header.USER_AGENT, "jdapp;android;10.5.0;;;appBuild/95837;ef/1;ep/{\"hdid\":\"JM9F1ywUPwflvMIpYPok0tt5k9kW4ArJEU3lfLhxBqw=\",\"ts\":"+ Instant.now().toEpochMilli()
+                        +",\"ridx\":-1,\"cipher\":{\"sv\":\"CJO=\",\"ad\":\"DNLsCzHsCzK2DzU4DwCmYG==\",\"od\":\"YJY5EJq0CzcnCwS5Czq3Zq==\",\"ov\":\"CzK=\",\"ud\":\"DNLsCzHsCzK2DzU4DwCmYG==\"},\"ciphertype\":5,\"version\":\"1.2.0\",\"appname\":\"com.jingdong.app.mall\"};jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; MI 9 Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.72 MQQBrowser/6.2 TBS/045947 Mobile Safari/537.36")//头信息，多个头信息多次调用此方法即可
                 .header(Header.ORIGIN, ORIGIN)
                 .header(Header.HOST, HOST)
                 .header(Header.CONNECTION, "keep-alive")
@@ -182,15 +188,23 @@ public class JdTryUtil {
 
                 .timeout(20000)//超时，毫秒;
                 .execute().body();
+        if ("Error request, response status: 403".equals(result2)) {
+            log.error("【异常】 403错误，休眠100秒后重新申请-获取商品" );
+            TimeUnit.SECONDS.sleep(100);
+            return new ArrayList<>();
+        }
         JSONObject result = JSONObject.parseObject(result2);
         JSONObject data = result.getJSONObject("data");
         JSONArray feedList = data.getJSONArray("feedList");
         for (int i = 0; i < feedList.size(); i++) {
             JdGoods goods = feedList.getJSONObject(i).toJavaObject(JdGoods.class);
+            System.out.println(goods.toString());
             if (ObjectUtil.isNotNull(goods) && validGoods(goods)) {
+
                 list.add(goods);
             }
         }
+
         return list;
 
     }
@@ -239,11 +253,14 @@ public class JdTryUtil {
 
 
     public static void main(String[] args) throws JSONException, InterruptedException {
+        System.out.println(    Instant.now().toEpochMilli());
         StopWatch sw = new StopWatch();
         sw.start();
         String cookie = "pt_key=AAJiitlYADB2Hfeiq2BRpPVkph2OSW_bPFBX0JStVe09lqWsH1erO6x8193lB2jDQg2sIYjbLvM; pt_pin=as15621009921;";
         Set<JdGoods> list = getAllGoodsList(cookie);
-        goodsService.saveBatch(list);
+      //  goodsService.saveBatch(list);
+
+
         list.stream().sorted(Comparator.comparing(JdGoods::getJdPrice).reversed())
                 .forEach(x -> {
                     if (allpuNum >= 300) {
