818cef5937c8033695ff7df1f3d8ec0b72411ba9
Wang.zx <wangzhengxiang@haier.com>
2022-11-21 10:14:13 +0800

    优化

--- ZeroGen/zeroGen-common/src/main/java/com/zero/common/utils/Arith.java
+++ ZeroGen/zeroGen-common/src/main/java/com/zero/common/utils/Arith.java
@@ -27,8 +27,8 @@ public class Arith
      */
     public static double add(double v1, double v2)
     {
-        BigDecimal b1 = new BigDecimal(Double.toString(v1));
-        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        BigDecimal b1 = new BigDecimal(String.valueOf(v1));
+        BigDecimal b2 = new BigDecimal(String.valueOf(v2));
         return b1.add(b2).doubleValue();
     }
 
@@ -40,8 +40,8 @@ public class Arith
      */
     public static double sub(double v1, double v2)
     {
-        BigDecimal b1 = new BigDecimal(Double.toString(v1));
-        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        BigDecimal b1 = new BigDecimal(String.valueOf(v1));
+        BigDecimal b2 = new BigDecimal(String.valueOf(v2));
         return b1.subtract(b2).doubleValue();
     }
 
@@ -53,8 +53,8 @@ public class Arith
      */
     public static double mul(double v1, double v2)
     {
-        BigDecimal b1 = new BigDecimal(Double.toString(v1));
-        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        BigDecimal b1 = new BigDecimal(String.valueOf(v1));
+        BigDecimal b2 = new BigDecimal(String.valueOf(v2));
         return b1.multiply(b2).doubleValue();
     }
 
@@ -85,8 +85,8 @@ public class Arith
             throw new IllegalArgumentException(
                     "The scale must be a positive integer or zero");
         }
-        BigDecimal b1 = new BigDecimal(Double.toString(v1));
-        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        BigDecimal b1 = new BigDecimal(String.valueOf(v1));
+        BigDecimal b2 = new BigDecimal(String.valueOf(v2));
         if (b1.compareTo(BigDecimal.ZERO) == 0)
         {
             return BigDecimal.ZERO.doubleValue();
@@ -107,7 +107,7 @@ public class Arith
             throw new IllegalArgumentException(
                     "The scale must be a positive integer or zero");
         }
-        BigDecimal b = new BigDecimal(Double.toString(v));
+        BigDecimal b = new BigDecimal(String.valueOf(v));
         BigDecimal one = BigDecimal.ONE;
         return b.divide(one, scale, RoundingMode.HALF_UP).doubleValue();
     }
