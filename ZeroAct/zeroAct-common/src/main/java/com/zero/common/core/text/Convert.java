28944ba36d3b73ab2c36858fc7f8f80c1b437340
wzx <wangzhengxiang@haier.com>
2023-03-07 13:50:34 +0800

    优化

--- ZeroAct/zeroAct-common/src/main/java/com/zero/common/core/text/Convert.java
+++ ZeroAct/zeroAct-common/src/main/java/com/zero/common/core/text/Convert.java
@@ -899,7 +899,7 @@ public class Convert
      */
     public static String toSBC(String input, Set<Character> notConvertSet)
     {
-        char c[] = input.toCharArray();
+        char[] c = input.toCharArray();
         for (int i = 0; i < c.length; i++)
         {
             if (null != notConvertSet && notConvertSet.contains(c[i]))
@@ -941,7 +941,7 @@ public class Convert
      */
     public static String toDBC(String text, Set<Character> notConvertSet)
     {
-        char c[] = text.toCharArray();
+        char[] c = text.toCharArray();
         for (int i = 0; i < c.length; i++)
         {
             if (null != notConvertSet && notConvertSet.contains(c[i]))
